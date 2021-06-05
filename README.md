54ce4587b45dddbe0a96387458de9b9be87c68d1
Taka710 <17783615+Taka710@users.noreply.github.com>
2021-06-05 13:23:16 +0900

    setup.pyの修正、readmeの変更
    ・install_requiresの記載
    ・readmeをrstからmdに変更

---
+++ README.md
@@ -0,0 +1,22 @@
+## @Taka710/normalize-japanese-addresses-py
+
+オープンソースの住所正規化ライブラリです。
+
+経産省の [IMI コンポーネントツール](https://info.gbiz.go.jp/tools/imi_tools/)のジオコーディングの仕組みから  
+インスピレーションをうけて開発された [@geolonia/normalize-japanese-addresses](https://github.com/geolonia/normalize-japanese-addresses)を  
+Pythonで利用できるように書き直したものです。
+
+## 注意
+
+以下の仕様は、元の [@geolonia/normalize-japanese-addresses](https://github.com/geolonia/normalize-japanese-addresses)を踏襲しています。  
+
+* この正規化エンジンは、住所の「名寄せ」を目的としており、たとえば京都の「通り名」は削除します。
+  * 郵便や宅急便などに使用される住所としては、問題ないと考えています。
+* この正規化エンジンは、町丁目及び小字レベルまでは対応していますが、それ以降については対応しておりません。
+* 住居表示が未整備の地域については全体的に苦手です。
+* 名寄せする住所は、[@geolonia/japanese-addresses](https://geolonia.github.io/japanese-addresses/api/ja)から都度取得しています。　
+* 漢数字と数字の変換については、[@geolonia/japanese-numeral](https://github.com/geolonia/japanese-numeral)をPythonに書き直して取り込んでいます。
+
+## ライセンス、利用規約
+
+ソースコードのライセンスは MIT ライセンスです。
--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import setuptools
+from distutils.core import setup
+from os import path
 
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-setuptools.setup(
+setup(
     name="normalize-japanese-addresses",
-    version="0.0.1",
+    version="0.0.1.3",
     author="Takahiro Hama",
     author_email="taka710.py@gmail.com",
     maintainer="Takahiro Hama",
     maintainer_email="taka710.py@gmail.com",
     description="Ported version of @geolonia/normalize-japanese-addresses",
     long_description=long_description,
-    long_description_content_type="text/x-rst",
+    long_description_content_type="text/markdown",
+    license='MIT License',
     url="https://github.com/Taka710/normalize-japanese-addresses-py",
     project_urls={
         "Bug Tracker": "https://github.com/Taka710/normalize-japanese-addresses-py/issues",
@@ -22,6 +25,11 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
     ],
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "requests",
+        "requests_cache",
+        "kanjize"
+    ],
     python_requires=">=3.8",
 )
