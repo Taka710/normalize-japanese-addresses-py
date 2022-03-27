c29308e4c852b9a08233638b53f4e26bf78df703
Taka710 <17783615+Taka710@users.noreply.github.com>
2022-03-27 13:54:57 +0900

    add api file read encode
    windowsで実行する際に、cp932のエンコードエラーが発生するため
    ファイル読み込みでutf-8のエンコードを指定するように変更

--- normalize_japanese_addresses/library/api.py
+++ normalize_japanese_addresses/library/api.py
@@ -8,8 +8,8 @@ def apiFetch(endpoint: str = ''):
         return requests.get(f'{endpoint}')
     elif endpoint.startswith('file'):
         filepath = urllib.parse.unquote(endpoint.replace("file://", ""))
-        with open(filepath) as fp:
-            # ファイから読み込んだ内容を擬似的にResponseオブジェクトに格納する
+        with open(filepath, encoding="utf-8") as fp:
+            # ファイルから読み込んだ内容を擬似的にResponseオブジェクトに格納する
             res = requests.Response()
             res._content = fp.read().encode("utf-8")
             return res
