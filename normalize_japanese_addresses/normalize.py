2f16f81a09cd763b59fe8c05512d8ee3ecb8db01
Taka710 <17783615+Taka710@users.noreply.github.com>
2023-03-29 21:15:34 +0900

    normalize.py refactor

--- normalize_japanese_addresses/normalize.py
+++ normalize_japanese_addresses/normalize.py
@@ -1,22 +1,26 @@
 import re
 import json
 import unicodedata
+import logging
+
+from typing import Tuple, Optional
 
 from .library.regex import getPrefectures, getPrefectureRegexes, getCityRegexes, replace_addr, normalizeTownName, match_banchi_go_pattern
 from .library.patchAddr import patchAddr
 from .library.utils import zen2han
 
-SPACE = ' '
-HYPHEN = '-'
+SPACE: str = ' '
+HYPHEN: str = '-'
 
 # japanese-addressesのendpoint
-endpoint = 'https://geolonia.github.io/japanese-addresses/api/ja'
+default_endpoint = 'https://geolonia.github.io/japanese-addresses/api/ja'
 
 # オプションのレベル設定
-level = 3
+default_level = 3
 
+logger = logging.getLogger(__name__)
 
-def normalize(address: str, **kwargs):
+def normalize(address: str, **kwargs) -> str:
     """
     住所正規化
     :param address: 住所
@@ -25,27 +29,85 @@ def normalize(address: str, **kwargs):
     """
 
     # オプションの設定
-    setOptions(kwargs)
+    level, endpoint = set_options(kwargs)
 
-    # 戻り値用
-    pref = ''
-    city = ''
-    town = ''
-    lat = None
-    lng = None
-    ref_level = 0
+    # 初期設定
+    addr, pref, city, town, lat, lng, ref_level = get_address_parts(address)
+
+    # 住所の前処理
+    addr = preprocessing_address(addr)
+
+    # 都道府県情報を取得
+    prefectures = json.loads(getPrefectures(endpoint).text)
+    prefectures_list: list = list(prefectures.keys())
+
+    # 都道府県の正規化
+    addr, pref = normalize_prefecture_names(
+        addr = addr, prefectures = prefectures, prefectures_list = prefectures_list, endpoint = endpoint)
+
+    # 市区町村の正規化
+    if pref != '' and level >= 2:
+        addr, city = normalize_city_names(
+            addr = addr, prefectures = prefectures, pref = pref)
+
+    # 町丁目以降の正規化
+    if city != '' and level >= 3:
+        addr, town, lat, lng = normalize_after_town_names(
+            addr = addr, pref = pref, city = city, endpoint = endpoint)
+
+    # 住所の後処理
+    addr = patchAddr(pref, city, town, addr)
+
+    # 戻り値のレベルを設定
+    ref_level = set_level(pref, city, town, ref_level)
+
+    return {
+        'pref': pref,
+        'city': city,
+        'town': town,
+        'addr': addr,
+        'lat': lat,
+        'lng': lng,
+        'level': ref_level,
+    }
+
+def set_options(options: dict) -> tuple:
+    """
+    オプションの設定
+    """
+    level = options.get("level", default_level)
+    endpoint = options.get("endpoint", default_endpoint)
+    return level, endpoint
+
+
+def get_address_parts(address: str) -> Tuple[str, str, str, str, Optional[float], Optional[float], int]:
+    """
+    住所の初期設定
+    """
+    # 初期化
+    pref: str = ''
+    city: str = ''
+    town: str = ''
+    lat: Optional[float] = None
+    lng: Optional[float] = None
+    ref_level: int = 0
 
     # 初期住所設定
-    addr = unicodedata.normalize('NFC', address)
+    addr: str = unicodedata.normalize('NFC', address)
+    return addr, pref, city, town, lat, lng, ref_level
 
-    # スペース変換
+def replace_spaces(addr: str) -> str:
+    """
+    全角スペースを半角スペースに置換する
+    """
     addr = addr.replace('　', SPACE).replace(' ', SPACE)
     addr = re.sub(' +', SPACE, addr)
+    return addr
 
-    # 全角の英数字は半角に変換
-    addr = zen2han(addr)
-
-    # 数字の後に紐づくハイフン類似文字をすべて半角ハイフンに変換
+def replace_hyphen_like_characters_after_digits(addr: str) -> str:
+    """
+    数字の後にあるハイフンのような文字をハイフンに置換する
+    """
     hyphen_iter = re.finditer(
         '([0-9０-９一二三四五六七八九〇十百千][-－﹣−‐⁃‑‒–—﹘―⎯⏤ーｰ─━])|([-－﹣−‐⁃‑‒–—﹘―⎯⏤ーｰ─━])[0-9０-９一二三四五六七八九〇十]',
         addr)
@@ -53,31 +115,63 @@ def normalize(address: str, **kwargs):
         from_value = m.group()
         replace_value = re.sub('[-－﹣−‐⁃‑‒–—﹘―⎯⏤ーｰ─━]', HYPHEN, from_value)
         addr = addr.replace(from_value, replace_value)
+    return addr
 
-    # 町丁目名以前のスペースはすべて削除
+def remove_spaces_before_town_city_district_name(addr: str) -> str:
+    """
+    町丁目名の前にあるスペースを削除する
+    """
     hyphen_iter = re.finditer('(.+)(丁目?|番([町地丁])|条|軒|線|([のノ])町|地割)', addr)
     for m in hyphen_iter:
         from_value = m.group()
         replace_value = from_value.replace(SPACE, '')
         addr = addr.replace(from_value, replace_value)
+    return addr
 
-    # 1番はじめに出てくるアラビア数字以前のスペースを削除
+def remove_leading_spaces_before_the_first_arabic_numeral(addr: str) -> str:
+    """
+    最初のアラビア数字の前にあるスペースを削除する
+    """
     hyphen_iter = re.finditer('.+?[0-9一二三四五六七八九〇十百千]-', addr)
     for m in hyphen_iter:
         from_value = m.group()
         replace_value = from_value.replace(SPACE, '')
         addr = addr.replace(from_value, replace_value)
         break
+    return addr
 
-    # 都道府県の正規化
-    response_prefs = getPrefectures(endpoint)
-    prefectures: dict = json.loads(response_prefs.text)
-    prefs: list = list(prefectures.keys())
-    for _pref, reg in getPrefectureRegexes(prefs):
-        if reg.match(addr):
-            pref = _pref
-            addr = addr[len(reg.match(addr)[0]):]
-            break
+def preprocessing_address(addr: str) -> str:
+    """
+    住所の前処理
+    """
+
+    # スペース変換
+    addr = replace_spaces(addr)
+
+    # 全角の英数字は半角に変換
+    addr = zen2han(addr)
+
+    # 数字の後に紐づくハイフン類似文字をすべて半角ハイフンに変換
+    addr = replace_hyphen_like_characters_after_digits(addr)
+
+    # 町丁目名以前のスペースはすべて削除
+    addr = remove_spaces_before_town_city_district_name(addr)
+
+    # 1番はじめに出てくるアラビア数字以前のスペースを削除
+    addr = remove_leading_spaces_before_the_first_arabic_numeral(addr)
+
+    return addr
+
+def normalize_prefecture_names(addr: str, prefectures: dict, prefectures_list: list, endpoint: str) -> str:
+    """
+    都道府県名を正規化する
+    """
+    pref = ''
+    for _pref, reg in getPrefectureRegexes(prefectures_list):
+            if reg.match(addr):
+                pref = _pref
+                addr = addr[len(reg.match(addr)[0]):]
+                break
 
     if pref == '':
         # 都道府県が省略されている
@@ -115,71 +209,70 @@ def normalize(address: str, **kwargs):
 
     # 都道府県が省略されている場合に都道府県を抽出（誤検知防止のため、省略
     if pref == '':
-        for _pref, reg in getPrefectureRegexes(prefs, True):
+        for _pref, reg in getPrefectureRegexes(prefectures_list, True):
             if reg.match(addr):
                 pref = _pref
                 addr = addr[len(reg.match(addr)[0]):]
                 break
+    
+    return addr, pref
 
-    # 市区町村の正規化
-    if pref != '' and level >= 2:
-        cities = prefectures[pref]
+def normalize_city_names(addr: str, prefectures: dict, pref: str) -> Tuple[str, str]:
+    """
+    市区町村名を正規化する
+    """
+    city = ''
+    cities = prefectures[pref]
 
-        for _city, reg in getCityRegexes(pref, cities):
-            match = reg.match(addr)
-            if match is not None:
-                city = _city
-                addr = addr[len(match.group()):]
-                break
+    for _city, reg in getCityRegexes(pref, cities):
+        match = reg.match(addr)
+        if match is not None:
+            city = _city
+            addr = addr[len(match.group()):]
+            break
+    
+    return addr, city
 
-    # 町丁目以降の正規化
-    if city != '' and level >= 3:
+def normalize_after_town_names(
+            addr: str,
+            pref: str,
+            city: str,
+            endpoint: str
+    ) -> Tuple[str, str, float, float]:
+    """
+    町名以降の住所を正規化する
+    """
+
+    town = ''
+    lat =  None
+    lng = None
+
+    banchiGoQueue = []
+    for pattern in match_banchi_go_pattern:
+        match = re.match(pattern, addr)
+        if match is not None:
+            banchiGoQueue.append(match[0])
+            addr = addr.replace(match[0], '')
         
-        banchiGoQueue = []
-        for pattern in match_banchi_go_pattern:
-            match = re.match(pattern, addr)
-            if match is not None:
-                banchiGoQueue.append(match[0])
-                addr = addr.replace(match[0], '')
-            
+    normalized = normalizeTownName(addr, pref, city, endpoint)
+    if normalized is not None:
+        _town = normalized['town']
+        town = _town['originalTown'] if 'originalTown' in _town else _town['town']
+        addr = normalized['addr']
+        lat = normalized['lat']
+        lng = normalized['lng']
 
-        normalized = normalizeTownName(addr, pref, city, endpoint)
-        if normalized is not None:
-            _town = normalized['town']
-            town = _town['originalTown'] if 'originalTown' in _town else _town['town']
-            addr = normalized['addr']
-            lat = normalized['lat']
-            lng = normalized['lng']
+    addr = ''.join(banchiGoQueue) + addr
+    
+    addr = replace_addr(addr)
 
-        addr = ''.join(banchiGoQueue) + addr
-        
-        addr = replace_addr(addr)
+    return addr, town, lat, lng
 
-    addr = patchAddr(pref, city, town, addr)
-
-    # 戻り値のレベルを設定
-    ref_level = ref_level + 1 if len(pref) > 0 else ref_level
-    ref_level = ref_level + 1 if len(city) > 0 else ref_level
-    ref_level = ref_level + 1 if len(town) > 0 else ref_level
-
-    return {
-        'pref': pref,
-        'city': city,
-        'town': town,
-        'addr': addr,
-        'lat': lat,
-        'lng': lng,
-        'level': ref_level,
-    }
-
-
-def setOptions(options: dict):
-    global level
-    global endpoint
-
-    # オプションの設定
-    if "level" in options:
-        level = options["level"]
-
-    if "endpoint" in options:
-        endpoint = options["endpoint"]
+def set_level(pref, city, town, ref_level):
+    """
+    住所のレベルを設定する
+    """
+    ref_level += len(pref) > 0
+    ref_level += len(city) > 0
+    ref_level += len(town) > 0
+    return ref_level
