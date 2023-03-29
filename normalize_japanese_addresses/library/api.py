530b1180e344d95c3a90b7d6282e8b64b386e046
Taka710 <17783615+Taka710@users.noreply.github.com>
2023-03-29 21:22:19 +0900

    api.py refactor

--- normalize_japanese_addresses/library/api.py
+++ normalize_japanese_addresses/library/api.py
@@ -5,13 +5,12 @@ import requests
 
 def apiFetch(endpoint: str = ''):
     if endpoint.startswith('http'):
-        return requests.get(f'{endpoint}')
+        return requests.get(endpoint)
     elif endpoint.startswith('file'):
         filepath = urllib.parse.unquote(endpoint.replace("file://", ""))
-        with open(filepath, encoding="utf-8") as fp:
-            # ファイルから読み込んだ内容を擬似的にResponseオブジェクトに格納する
+        with open(filepath, 'rb') as fp:
             res = requests.Response()
-            res._content = fp.read().encode("utf-8")
+            res.raw = fp
             return res
     else:
         raise ValueError("Invalid endpoint type")
